cbe719b560258c6690401e958174d35a685ab693
Hamada <defjamvk@gmail.com>
2020-02-29 16:03:43 +0100

    commit woking on poduct methods
    commit woking on poduct methods

--- server/routes/products.js
+++ server/routes/products.js
@@ -5,9 +5,24 @@ const Product = require("../model/product");
 
 // GET Method
 router.get("/", function(req, res, next) {
-  res.status(200).json({
-    message: "from product in get method"
-  });
+  Product.find()
+    .exec()
+    .then(allProducts => {
+      console.log(allProducts);
+      if (allProducts.length >= 0) {
+        res.status(200).json(allProducts);
+      } else {
+        res.status(404).json({
+          message: "Array dont has products"
+        });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
 // POST Method
@@ -21,29 +36,45 @@ router.post("/", function(req, res, next) {
     .save()
     .then(result => {
       console.log(result);
+      res.status(201).json({
+        message: "Product posted with status: 201",
+        createdProduct: result
+      });
     })
     .catch(err => {
       console.log(err);
+      res.status(500).json({ error: err });
     });
-  res.status(201).json({
-    message: "product posted with status 201",
-    createdProduct: product
-  });
 });
 
 // GET Method FOR ID PRODUCT
 router.get("/:productId", function(req, res, next) {
   const id = req.params.productId;
-  if (id === "special") {
-    res.status(200).json({
-      message: "from GET SPECIAL products url",
-      id: id
+  Product.findById(id)
+    .exec()
+    .then(documente => {
+      console.log("From Mongo:", documente);
+      if (documente) {
+        res.status(200).json(documente);
+      } else {
+        res.status(404).json({ message: "Product Id Not Exist" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
     });
-  } else {
-    res.status(200).json({
-      message: "you passed an ID"
-    });
-  }
+
+  // if (id === "special") {
+  //   res.status(200).json({
+  //     message: "from GET SPECIAL products url",
+  //     id: id
+  //   });
+  // } else {
+  //   res.status(200).json({
+  //     message: "you passed an ID"
+  //   });
+  // }
 });
 router.patch("/:productId", function(req, res, next) {
   res.status(200).json({
@@ -52,9 +83,18 @@ router.patch("/:productId", function(req, res, next) {
 });
 
 router.delete("/:productId", function(req, res, next) {
-  res.status(200).json({
-    message: "delete product "
-  });
+  const id = req.params.productId;
+  Product.remove({ _id: id })
+    .exec()
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
 module.exports = router;
